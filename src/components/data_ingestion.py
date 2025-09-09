407439e23bc178f1487d92d292b39419be2a36e1
AyanAhmad710 <sheikhayanahmad710@gmail.com>
2025-09-09 05:34:54 +0500

    Data Transformation is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,8 +5,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-@dataclass
+@dataclass #Use for making clas which only have variables
 class DataIngestionConfig:
     train_data_path=os.path.join('artifact','train.csv')
     test_data_path=os.path.join('artifact','test.csv')
@@ -23,7 +25,7 @@ class DataIngestion:
             df=pd.read_csv('C:/Machine Learning/ML_projects/ml_project/notebook/data/StudentsPerformance.csv')
             logging.info("Data has been read and transfered into the DataFrame")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #It takes only the folder name and then create it
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train Test Split Initiated")
@@ -47,4 +49,6 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
